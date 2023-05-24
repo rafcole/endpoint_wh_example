4ffde96aaf1d0f0cf33a8176ab7470f5ff0186ec
judah <landzbej@gmail.com>
2023-05-23 21:59:01 -0400

    demo

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
 const express = require('express')
 const app = express()
 const port = 8000
+app.use(express.json())
 
+// const db = require('./mongo.js')
+
+// const mongo = require('mongodb');
+// const client = new mongo.MongoClient('mongodb://localhost:27017', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// var db;
+// // var animals;
+// client.connect(() => {
+//   db = client.db('test');
+//   animals = db.collection('test2');
+//   console.log('mongoDB connected')
+// });
+
+
+// addAnimal = () => {
+//   const animalName = prompt('Name: ');
+//   const ageAnimal = prompt('Age: ');
+//   animals.insertOne(
+//     {
+//       nameAnimal: animalName,
+//       age: ageAnimal,
+//     },
+//     (err) => {
+//       if (err) {
+//         console.log('Error');
+//       } else {
+//         console.log('Added!');
+//       }
+//     }
+//   );
+// };
+// addAnimal()
 
 const {MongoClient} = require('mongodb');
-
+let db;
 
 async function main() {
   const uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.0"
 
   const client = new MongoClient(uri);
-
+let conn;
   try {
-    await client.connect();
+   conn = await client.connect();
 
     await listDatabases(client);
   } catch (e) {
       console.error(e);
-  } finally {
-    await client.close();
-  }
+  } 
+  // finally {
+  //   await client.close();
+  // }
+  db = conn.db("test")
 }
 
 main().catch(console.error);
@@ -44,6 +82,23 @@ app.post('/bin/my_hardcoded_endpoint', (req, res) => {
 // stub
 app.post('/bin/:id', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/payload', async (req, res) => {
+  let collection = await db.collection("test2");
+  await collection.insertOne(req.body);
+  res.send("ok");
+})
+
+app.get('/requests', async (req, res) => {
+  // res.send('Hello World!')
+  let collection = await db.collection("test2");
+  
+  let list = await collection.find().toArray();
+  // let formatList = list.map(obj => JSON.stringify(obj, null, 2));
+  // list.forEach();
+  console.log(list);
+  res.send(list);
 })
 
 // display all request data received at this entry point
---
+++ mongo.js
@@ -0,0 +1,16 @@
+// const { MongoClient } = require("mongodb");
+
+// const connectionString = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.0";
+
+// const client = new MongoClient(connectionString);
+
+// let conn;
+// try {
+//   conn = await client.connect();
+// } catch(e) {
+//   console.error(e);
+// }
+
+// let db = conn.db("test");
+
+// export default db;
